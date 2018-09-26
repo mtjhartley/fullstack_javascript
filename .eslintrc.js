242a84de090c6520eb7717e44f28063236695d7e
Michael Hartley <mtjhartley@gmail.com>
2018-09-25 18:49:51 -0700

    server with http

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,9 +16,8 @@ module.exports = {
     },
     "plugins": [ "react" ],
     "rules": {
-      "indent": ["error", 2],
+      "indent": ["error", 4],
       "linebreak-style": ["error","unix"],
-      "quotes": ["error","single"],
       "semi": ["error","always"],
       "no-console": ["warn", { "allow": ["info", "error"] }]
     }
---
+++ server.js
@@ -0,0 +1,13 @@
+import http from 'http';
+import https from 'https';
+import { setTimeout } from 'timers';
+
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write("I can stream!\n");
+        res.end();
+    }, 3000);
+});
+
+server.listen(8080);
